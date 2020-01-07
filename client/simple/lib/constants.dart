e48fefaec21be4d08f9830a70919c30a9dd6046e
ged <winwisely99@gmail.com>
2020-01-07 22:15:22 +0100

    make import

--- client/simple/lib/constants.dart
+++ client/simple/lib/constants.dart
@@ -9,6 +9,6 @@ const APP_DESCRIPTION = 'An app showcasing Flutter components, with '
     'side-by-side source code view.'
     '\n\nDeveloped by X.Wei.';
 const GOOGLEPLAY_URL =
-    'https://play.google.com/store/apps/details?id=io.github.x_wei.flutter_catalog';
-const GITHUB_URL = 'https://github.com/X-Wei/flutter_catalog';
-const AUTHOR_SITE = 'http://x-wei.github.io';
+    'https://play.google.com/store/apps/details?id=org.getcouragenow.catalog';
+const GITHUB_URL = 'https://github.com/getcouragenow/catalogue';
+const AUTHOR_SITE = 'https://getcouragenow.org/';
